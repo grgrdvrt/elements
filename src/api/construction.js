65d944f99d71f584af36e9e07f5850f6c1944259
grgrdvrt <gdivaret@gmail.com>
2020-05-24 09:08:16 +0200

    simplify construction

--- src/api/construction.js
+++ src/api/construction.js
@@ -4,36 +4,12 @@ function updateObject(object, timeStamp){
     if(object.lastUpdated >= timeStamp){
         return;
     }
-    if(Array.isArray(object)){
-        object.forEach(obj => updateObject(obj, timeStamp));
+    for(let input of object.inputs){
+        updateObject(input, timeStamp);
     }
-    else if(object.construction !== undefined){
-        for(let helper of object.helpers){
-            updateObject(helper, timeStamp);
-        }
-        for(let input of object.inputs){
-            updateObject(input, timeStamp);
-        }
-        object.update(object.input, object.output, object.helpers, timeStamp);
-        object.lastUpdated = timeStamp;
+    for(let helper of object.helpers){
+        updateObject(helper, timeStamp);
     }
-}
-
-export function makeConstructor(description, types, init, update){
-    const spreadTypes = initTypes(types)
-    const constructor = function(...args){
-        const objects = init(...spreadTypes(args))
-        Object.assign(objects.output, {
-            ...objects.input,
-            construction:new Construction({
-                description,
-                ...objects,
-                update:update
-            })
-        })
-        return objects.output;
-    }
-    constructor.types = types;
-    constructor.spreadTypes = spreadTypes;
-    return constructor;
+    object.update(object, timeStamp);
+    object.lastUpdated = timeStamp;
 }
